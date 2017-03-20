25022f4d35890196682541c12ce66428eefb591d
wajdibr <benrabah.wajdi@gmail.com>
2017-03-20 11:38:10 +0100

    interaction in augmented dimension using virtual buttons and vuforia/android sdk
    [FIX] previous push

---
+++ Assets/scripts/vbscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbscript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vButtonObject;
+	private GameObject zombie;
+
+	// Use this for initialization
+	void Start () {
+		vButtonObject = GameObject.Find ("actionbutton");
+		zombie = GameObject.Find ("zombie");
+
+		vButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+		Debug.Log ("button pressed");
+		zombie.GetComponent<Animation> ().Play ();
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+		Debug.Log ("button released");
+		zombie.GetComponent<Animation> ().Stop ();
+
+	
+	}
+
+
+}
